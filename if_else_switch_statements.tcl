1553633fe538ea065e893288baecea8dacc0947d
Alexsandro Farias de França <69731110+dejavudf@users.noreply.github.com>
2024-10-14 13:49:24 -0300

    Update if_else_switch_statements.tcl

--- if_else_switch_statements.tcl
+++ if_else_switch_statements.tcl
@@ -1,9 +1,9 @@
-# ayengec TCL scripts examples
+
 
 set lang "SV"
 set meth "UVM"
 
-#only one space between the word1 and word2 "} {"
+
 if { $lang == "SV" } {
     puts "Welcome. Systemverilog is completely supported. Create your project."
     if {$meth == "UVM"} {
@@ -16,14 +16,6 @@ if { $lang == "SV" } {
 } else {
     puts "Other languages are not supported by this tool."
 }
-# Here is so critical. You have to break one space between { or } and expressions; no Enter, Tab, newline etc...
-# Be care there should be one spcae every { and else/elseif/if statements
-
-##################### OUTPUTS ###############################################
-# Welcome. Systemverilog is completely supported. Create your project.
-# Yes. UVM is supported.
-
-##################### SWITCH CASE STATEMENT #################################
 
 set langHDL "SV" # "SystemC" for other output
 switch $langHDL \
@@ -32,6 +24,3 @@ switch $langHDL \
 "SV"         "puts {Selected Language is Systemverilog}" \
 "TCL"        "puts {Selected Language is TCL}" \
 "default"    "puts {It's not supported by Vivado}";
-##################### OUTPUTS ###############################################
-# Selected Language is Systemverilog          -> While langHDL="SV"
-# It's not supported by Vivado                -> While langHDL="SystemC"
