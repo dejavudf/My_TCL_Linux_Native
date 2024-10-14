f1b9d3bf4c95f10853f42602f88baef1371031ef
Alexsandro Farias de França <69731110+dejavudf@users.noreply.github.com>
2024-10-14 13:52:17 -0300

    Update and rename TCL script to remove all white spaces in string.tcl to remove_spaces_string.tcl

--- remove_spaces_string.tcl
+++ remove_spaces_string.tcl
@@ -1,9 +1,3 @@
-#Written by : Udaya Shankar S
-#dated: 11.11.2022
-#TCL script to remove all white spaces in string.
-#Version 1.0
-
-
 #Method1
 set str "Tcl is a cross platform language with full unicode support."
 set replaced_string [regsub -all " " $str ""]
@@ -14,7 +8,6 @@ set new_string [string map {" " ""} $str]
 puts $new_string
 
 #Method 3
-#set str "Tcl Scripting Language"
 set l1 [split $str " "]
 set l [llength $str]
 set y [list]
