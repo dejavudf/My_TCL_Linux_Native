615be97a9c057d3db5df1d5405e2094af7aa5182
Alexsandro Farias de França <69731110+dejavudf@users.noreply.github.com>
2024-10-14 14:05:15 -0300

    Update and rename TCL script to find two list are equal or sublist or superlists.tcl to find_two_list_sublist.tcl

--- find_two_list_sublist.tcl
+++ find_two_list_sublist.tcl
@@ -1,17 +1,4 @@
-#Written by : Udaya Shankar S
-#dated: 11.11.2022
-#TCL script to find two list are equal or sublist or superlists.
-#Version 1.0
 
-#set A [list] ; set B [list] 
-#set A [list 1 2 3] ; set B [list]
-#set A [list] ; set B [list 1 2 3]
-#set A [list 1 2 3] ; set B [list 1 2 3 4 5]
-#set A [list 3 4 5] ; set B [list 1 2 3 4 5]
-#set A [list 3 5 4] ; set B [list 1 2 3 4 5]
-#set A [list 1 3 2 4 ] ; set B [list 2 3 4]
-#set A [list 1 2 3 4 5] ; set B [list 2 3 4]
-#set A [list 1 2 4] ; set B [list 1 2 3 4 5]
 set A [list 1 2 3] ; set B [list 1 3 2]
 puts "x:[set x [regsub -all " " $A ""]]" ; 
 puts "y:[set y [regsub -all " " $B ""]]"
