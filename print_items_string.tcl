ccf4a6c03a1fc552e2a4d27a291d4c6b1df8bca7
Alexsandro Farias de França <69731110+dejavudf@users.noreply.github.com>
2024-10-14 13:52:42 -0300

    Update and rename TCL script to print items in list in reverse order without lreverse.tcl to print_items_string.tcl

--- print_items_string.tcl
+++ print_items_string.tcl
@@ -1,7 +1,3 @@
-#Written by: Udaya Shankar S
-#Dated: 8.11.2022
-#TCL script to print the items in the list in the reverse order without using "lreverse" 
-#Version:1.0
 
 set in {a z d e f}
 set l [llength $in]
