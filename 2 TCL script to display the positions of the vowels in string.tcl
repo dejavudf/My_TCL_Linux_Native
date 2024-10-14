5c1840ed19a61a9699389da548a9f7aefda37fef
Alexsandro Farias de França <69731110+dejavudf@users.noreply.github.com>
2024-10-14 13:38:12 -0300

    Update 2 TCL script to display the positions of the vowels in string.tcl

--- 2 TCL script to display the positions of the vowels in string.tcl
+++ 2 TCL script to display the positions of the vowels in string.tcl
@@ -1,9 +1,3 @@
-#Written by: Udaya Shankar S
-#Dated: 8.11.2022
-#TCL script to display the positions of the vowels present in the input string
-#Version:1.0
-
-
 set in "MANPOWER"
 for {set i 0} {$i < [string length $in]} { incr i} {
     if { [string equal -nocase [string index $in $i] "a"] == 1 } {
