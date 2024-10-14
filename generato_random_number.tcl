a6cad2d902ac2c3e997239d091ab420df45084cd
Alexsandro Farias de França <69731110+dejavudf@users.noreply.github.com>
2024-10-14 13:59:10 -0300

    Update and rename Script for display only the random generated numbers comes within the specified range.tcl to generato_random_number.tcl

--- generato_random_number.tcl
+++ generato_random_number.tcl
@@ -1,7 +1,3 @@
-#Written by: Udaya Shankar S
-#Dated: 3.10.2022
-# Tcl procedure to display only the random generated numbers comes within the specified range
-#Version 1.0
 
 # Method 1
 proc random {} {
