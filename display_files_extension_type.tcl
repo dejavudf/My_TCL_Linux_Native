4ed3d71b2adea6cca7f1f3c83b8c8c146e3b8a63
Alexsandro Farias de França <69731110+dejavudf@users.noreply.github.com>
2024-10-14 13:46:43 -0300

    Update and rename display all the files with extension of a particular type only in groups.tcl to display_files_extension_type.tcl

--- display_files_extension_type.tcl
+++ display_files_extension_type.tcl
@@ -1,8 +1,3 @@
-#written by: Udaya Shakar S
-#Dated: 28.11.2022
-#TCL script to display all the files with extension of a particular type only in groups(*.txt or *.tcl or *.doc or *.csv) in a folder
-#Version 1.0
-
 
 set f [glob *]
 puts $f
