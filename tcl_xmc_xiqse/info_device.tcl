fa3b9f3415c60e28ce2f557bfd49ce23f354d802
Alexsandro Farias de França <69731110+dejavudf@users.noreply.github.com>
2025-05-02 13:05:18 -0300

    Create info_device.tcl

---
+++ tcl_xmc_xiqse/info_device.tcl
@@ -0,0 +1,11 @@
+#print info do dispositivo
+set var VAR_FINAL
+puts "IP: $deviceIP"
+puts "ID Vendor: $vendor"
+puts "ID Device Vendor: $deviceId"
+puts "Device Type: $deviceType"
+puts "Device Family: $family"
+puts "Firmware/SO Version $deviceSoftwareVer"
+puts "MIB SysOid: $deviceSysOid"
+puts $VAR_FINAL
+
