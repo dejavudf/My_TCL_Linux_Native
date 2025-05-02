eea12ad83d133c9b181dee23dd8e57934e5b4d2c
Alexsandro Farias de França <69731110+dejavudf@users.noreply.github.com>
2025-05-02 20:24:49 -0300

    Update syslog_switches.tcl

--- tcl_xmc_xiqse/syslog_switches.tcl
+++ tcl_xmc_xiqse/syslog_switches.tcl
@@ -1,9 +1,18 @@
-### script version 1.0 - alexsandro
+#############################################################################
+# KLABIN / CAPGEMINI - XMC and XIQ-SE TCL Scripts
+#
+# Script        : Configure syslog - Switches
+# Revision      : 1.0
+# Last Updated  : 02/05/2025
+# Author: Alexsandro Farias de França (alexsandro.franca@capgemini.com)
+# Status: Completed for Extreme EXOS, Cisco, HP Comware and Enterasys
+############################################################################
+
 ###declara variaveis dos servidores syslog (total 3)
 set var VAR_SRV_XMC 10.22.0.135
 set var VAR_SRV_XIQSE 10.95.0.52
 #set var VAR_SRV_SIEM 10.150.0.19
-set var VAR_FINAL "Fim do Scripts"
+set var VAR_FINAL "Fim do Script"
 
 #print info do dispositivo antes de inicar o script (ajuda no diag)
 puts "IP: $deviceIP"
@@ -81,14 +90,16 @@ IF ([string equal $family "HP"]) THEN
     CLI system-view
     CLI info-center enable
     CLI undo info-center loghost $VAR_SRV_XMC
-    CLI undo info-center loghost  $VAR_SRV_XIQSE
-    #CLI undo info-center loghost $VAR_SRV_SIEM    
-    CLI info-center loghost $VAR_SRV_XMC5 facility local0
-    CLI info-center loghost $VAR_SRV_XIQS facility local0
-    CLI info-center loghost $VAR_SRV_XMC5 channel loghost level information
-    CLI info-center loghost $VAR_SRV_XIQS channel loghost level information
-    #CLI info-center loghost $VAR_SRV_SIEM channel loghost level information
+    CLI undo info-center loghost $VAR_SRV_XIQSE
+    #CLI undo info-center loghost $VAR_SRV_SIEM
+    CLI info-center loghost $VAR_SRV_XMC channel loghost facility local0
+    CLI info-center loghost $VAR_SRV_XMC facility local0
+    CLI info-center loghost $VAR_SRV_XIQSE channel loghost facility local0
+    CLI info-center loghost $VAR_SRV_XIQSE facility local0
+    #CLI info-center loghost $VAR_SRV_SIEM channel loghost facility local0
     #CLI info-center loghost $VAR_SRV_SIEM facility local0
+    CLI info-center source default channel loghost log level informational state on
+    CLI info-center source default loghost level informational
     CLI save
     CLI y
     CLI
